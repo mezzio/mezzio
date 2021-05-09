0d9f6d6420261d8bc6fd3a683bd28a66f155ebf9
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-05-09 20:05:57 +0200

    bugfix: allow `ArrayAccess` for `NotFoundHandlerFactory`
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- src/Container/NotFoundHandlerFactory.php
+++ src/Container/NotFoundHandlerFactory.php
@@ -23,7 +23,7 @@ class NotFoundHandlerFactory
     public function __invoke(ContainerInterface $container) : NotFoundHandler
     {
         $config = $container->has('config') ? $container->get('config') : [];
-        Assert::isMap($config);
+        Assert::isArrayAccessible($config);
 
         $renderer = $container->has(TemplateRendererInterface::class)
             ? $container->get(TemplateRendererInterface::class)
