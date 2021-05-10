dd8238baa2bc2566593095668d067a583e7ebef4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:01:25 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/Response/ServerRequestErrorResponseGeneratorTest.php
+++ test/Response/ServerRequestErrorResponseGeneratorTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace MezzioTest\Response;
