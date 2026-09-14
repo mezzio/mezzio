6346d90f7470b985667c3618f2ce6247fd6e1304
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-09-14 08:48:46 +0700

    fix ruleset definition
    Signed-off-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

--- structarmed.php
+++ structarmed.php
@@ -6,20 +6,30 @@ use Boundwize\StructArmed\Architecture;
 use Boundwize\StructArmed\Preset\Preset;
 
 return Architecture::define()
-    ->layer('Core', 'src')
+    ->layer('Core', [
+        'src/Application.php',
+        'src/MiddlewareContainer.php',
+        'src/MiddlewareFactory.php',
+        'src/MiddlewareFactoryInterface.php',
+        'src/constants.php',
+        'src/constants.legacy.php',
+    ])
+    ->layer('Config', 'src/ConfigProvider.php')
     ->layer('Exception', 'src/Exception')
     ->layer('Response', 'src/Response')
     ->layer('Handler', 'src/Handler')
     ->layer('Middleware', 'src/Middleware')
     ->layer('Router', 'src/Router')
+    ->layerPattern('Router', '/^Mezzio\\\\Router\\\\.*$/')
     ->layer('Container', 'src/Container')
     ->withPresets(Preset::PSR4(), Preset::CODEQUALITY())
     ->ruleset([
+        'Config'     => ['Container', '+Core', '+Handler'],
         'Exception'  => [],
         'Response'   => [],
         'Handler'    => ['Response'],
         'Middleware' => ['Core', 'Exception', 'Response'],
         'Router'     => ['Core'],
         'Core'       => ['Exception', 'Middleware', 'Router'],
-        'Container'  => ['+Core', '+Handler'],
+        'Container'  => ['Config', '+Core', '+Handler'],
     ]);
